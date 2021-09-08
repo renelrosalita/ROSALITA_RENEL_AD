491a08f465a12eeb7df0d6cc127e55275a428bed
Renel Rosalita <renel.rosalita.20@usjr.edu.ph>
2021-09-08 18:22:13 +0800

    Added files

---
+++ 2_JS/LoopsTriangle.js
@@ -0,0 +1,2 @@
+for (var result = "*"; result.length <= 6; result = result + "*")
+    console.log(result);
---
+++ 2_JS/VariableTypes.js
@@ -0,0 +1,17 @@
+var array = [1, 2, 3, 4, 5, 6];
+var object = { one: 1, "two": "two", three: [3] };
+var c;
+var d = null;
+var str = "HELLO I AM STRING";
+console.log(array);
+console.log(`The variable ${array} is of type ${typeof array}`);
+console.log(object);
+console.log(`The variable ${object} is of type ${typeof object}`);
+console.log(c);
+console.log(`The variable ${c} is of type ${typeof c}`);
+console.log(d);
+console.log(`The variable ${d} is of type ${typeof d}`);
+console.log(object['one']);
+console.log(`The variable ${object['one']} is of type ${typeof object['one']}`);
+console.log(str);
+console.log(`The variable ${str} is of type ${typeof str}`);
